76d81217db65ed4c67ae24705378c9212c90d711
Jimmy Cook <jimmy.cook.1993@gmail.com>
2016-05-22 17:23:43 +0100

    testing heroku, stripped out all routes for now

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,19 +3,21 @@
 use App\Steam\Client as Client;
 
 Route::get('/', function (Client $client) {
-    $steamid = $client->getSteamId('Jimmypq');
-    $user = $client->getPlayerSummary($steamid);
+    return 'hello, world';
+    
+    // $steamid = $client->getSteamId('Jimmypq');
+    // $user = $client->getPlayerSummary($steamid);
 
-    return view('home', ['user' => $user]);
+    // return view('home', ['user' => $user]);
 });
 
 
 Route::group(['middlewear' => 'api', 'prefix' => 'api'], function() {
 
-	Route::get('player/{name}', function ($name, Client $client) {
-        $steamid = $client->getSteamId('Jimmypq');
-        $user = $client->getPlayerSummary($steamid);
-        return json_encode($user);
-	});
+	// Route::get('player/{name}', function ($name, Client $client) {
+    //     $steamid = $client->getSteamId('Jimmypq');
+    //     $user = $client->getPlayerSummary($steamid);
+    //     return json_encode($user);
+	// });
 
 });
